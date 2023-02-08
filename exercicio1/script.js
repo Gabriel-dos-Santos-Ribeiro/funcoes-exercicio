a74466f6c57df0bf98a7752b57a92badc4bf4d7d
Gabriel dos Santos Ribeiro <gabrejcguga@gmail.com>
2023-02-07 21:22:51 -0300

    Resolução exercicios de funções

---
+++ exercicio1/script.js
@@ -0,0 +1,69 @@
+// A)
+
+function mensagemOla(nome){
+    const frase = `Olá,  ${nome} !`
+
+     console.log(frase)
+}
+
+mensagemOla('Gabriel')
+
+// Função Não nomeada
+/*
+const mensagemOla = function(nome){
+    const frase = `Olá, ${nome} !`
+
+    console.log(frase)
+}
+
+mensagemOla('Gabriel')
+
+*/
+
+//  B) 
+/*
+function tabuada(numero){
+    const numero1 = numero 
+    console.log(`${numero1} * 1 é : ${numero1 * 1}`)
+    console.log(`${numero1} * 2 é : ${numero1 * 1}`)
+    console.log(`${numero1} * 3 é : ${numero1 * 1}`)
+    console.log(`${numero1} * 4 é : ${numero1 * 1}`)
+    console.log(`${numero1} * 5 é : ${numero1 * 1}`)
+    console.log(`${numero1} * 6 é : ${numero1 * 1}`)
+    console.log(`${numero1} * 7 é : ${numero1 * 1}`)
+    console.log(`${numero1} * 8 é : ${numero1 * 1}`)
+    console.log(`${numero1} * 9 é : ${numero1 * 1}`)
+    console.log(`${numero1} * 10 é : ${numero1 * 1}`)
+}
+
+tabuada(1)
+tabuada(2)
+tabuada(3)
+tabuada(4)
+tabuada(5)
+tabuada(6)
+tabuada(7)
+tabuada(8)
+tabuada(9)
+tabuada(10)
+*/
+// C)
+/*
+const numero1 = (numero) => {
+    const numero1 = numero
+    console.log(`${numero1} * 1 é : ${numero1 * 1}`)
+    console.log(`${numero1} * 2 é : ${numero1 * 2}`)
+    console.log(`${numero1} * 3 é : ${numero1 * 3}`)
+    console.log(`${numero1} * 4 é : ${numero1 * 4}`)
+    console.log(`${numero1} * 5 é : ${numero1 * 5}`)
+    console.log(`${numero1} * 6 é : ${numero1 * 6}`)
+    console.log(`${numero1} * 7 é : ${numero1 * 7}`)
+    console.log(`${numero1} * 8 é : ${numero1 * 8}`)
+    console.log(`${numero1} * 9 é : ${numero1 * 9}`)
+    console.log(`${numero1} * 10 é : ${numero1 * 10}`)
+
+}
+
+numero1(2)
+*/
+
